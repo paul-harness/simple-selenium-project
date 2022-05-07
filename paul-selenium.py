8eea840ca3742c03e5a9403bc3b38385533f9d7f
paul-harness <56152762+paul-harness@users.noreply.github.com>
2022-05-07 08:17:33 -0400

    Update paul-selenium.py

--- paul-selenium.py
+++ paul-selenium.py
@@ -1,6 +1,21 @@
-from selenium.webdriver.firefox.options import Options
+import time
+from selenium import webdriver
 from pyvirtualdisplay import Display
 
-display = Display(visible=0, size=(800, 600))
+display = Display(visible=0, size=(800, 800))
 display.start()
+
 driver = webdriver.Firefox()
+driver.get('http://www.cnblogs.com/')
+
+time.sleep(5)
+title = driver.title
+print(title.encode('utf-8'))
+
+# html=driver.page_source
+# print(html)
+
+# Release memory
+driver.close()
+driver.quit()
+display.stop()

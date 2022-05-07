97497da2c0ea4b54dab730d2a32f5342728c3fa4
paul-harness <56152762+paul-harness@users.noreply.github.com>
2022-05-07 13:02:41 -0400

    Update and rename main.py to paul-selenium.py

--- main.py
+++
@@ -1,3 +0,0 @@
-from scraper import Scraper
- 
-pretty_html = SCRAPER.scrape(link).prettify()
---
+++ paul-selenium.py
@@ -0,0 +1,23 @@
+# coding:utf-8
+import time
+from selenium import webdriver
+from pyvirtualdisplay import Display
+
+display = Display(visible=0, size=(800, 800))
+display.start()
+
+driver = webdriver.Chrome()
+
+driver.get('http://www.cnblogs.com/')
+time.sleep(5)
+
+title = driver.title
+print(title.encode('utf-8'))
+
+# html=driver.page_source
+# print(html)
+
+# Release memory
+driver.close()
+driver.quit()
+display.stop()

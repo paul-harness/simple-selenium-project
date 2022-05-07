f0a6de8ae03531a88dc53b6ded27a7527c9b063a
paul-harness <56152762+paul-harness@users.noreply.github.com>
2022-05-07 09:50:20 -0400

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,25 +1,12 @@
 from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.keys import Keys
 
-
-def selectOption(select,texttoselect):
-    options = select.find_elements_by_tag_name("option")
-    if options:
-        for o in options:
-            if o.text == texttoselect:
-                o.click()
-
-browser = webdriver.Firefox()
-browser.get('http:///www.seek.co.nz')
-
-keywords = browser.find_element_by_id('Keywords')
-#require initial backspace to delete default text
-keywords.send_keys(Keys.BACK_SPACE + "tech")
-industries = browser.find_element_by_id('catindustry')
-selectOption(industries,'Legal')
-search = browser.find_element_by_id('DoSearch')
-search.submit()
-#this is an example to visually demonstrate controlling the browser
-#however normally you would close the browser instance  with:
-#browse
+driver = webdriver.Firefox()
+driver.get("http://www.python.org")
+assert "Python" in driver.title
+elem = driver.find_element_by_name("q")
+elem.clear()
+elem.send_keys("pycon")
+elem.send_keys(Keys.RETURN)
+assert "No results found." not in driver.page_source
+driver.close()

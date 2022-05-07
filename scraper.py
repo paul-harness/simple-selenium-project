672fb1a0081bd6737383e11f6aef81e1946e2ae9
paul-harness <56152762+paul-harness@users.noreply.github.com>
2022-05-07 09:47:39 -0400

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,6 +1,25 @@
-from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.keys import Keys
 
-options = FirefoxOptions()
-options.add_argument("--headless")
-driver = webdriver.Firefox(options=options)
-driver.get("https://pythonbasics.org")
+
+def selectOption(select,texttoselect):
+    options = select.find_elements_by_tag_name("option")
+    if options:
+        for o in options:
+            if o.text == texttoselect:
+                o.click()
+
+browser = webdriver.Firefox()
+browser.get('http:///www.seek.co.nz')
+
+keywords = browser.find_element_by_id('Keywords')
+#require initial backspace to delete default text
+keywords.send_keys(Keys.BACK_SPACE + "tech")
+industries = browser.find_element_by_id('catindustry')
+selectOption(industries,'Legal')
+search = browser.find_element_by_id('DoSearch')
+search.submit()
+#this is an example to visually demonstrate controlling the browser
+#however normally you would close the browser instance  with:
+#browse

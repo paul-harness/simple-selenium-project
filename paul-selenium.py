6c910853f9249331d012d26d399d39e8fe791869
paul-harness <56152762+paul-harness@users.noreply.github.com>
2022-05-07 13:40:23 -0400

    Update paul-selenium.py

--- paul-selenium.py
+++ paul-selenium.py
@@ -1,23 +1,47 @@
-# coding:utf-8
-import time
+"""
+A simple selenium test example written by python
+"""
+
+import unittest
 from selenium import webdriver
-from pyvirtualdisplay import Display
+from selenium.common.exceptions import NoSuchElementException
 
-display = Display(visible=0, size=(800, 800))
-display.start()
+class TestTemplate(unittest.TestCase):
+    """Include test cases on a given url"""
 
-driver = webdriver.Chrome()
+    def setUp(self):
+        """Start web driver"""
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--disable-gpu')
+        chrome_options.add_argument('--disable-dev-shm-usage')
+        chrome_options.add_argument("--window-size=1920,1080")
+        self.driver = webdriver.Chrome(options=chrome_options)
+        self.driver.implicitly_wait(10)
 
-driver.get('http://www.cnblogs.com/')
-time.sleep(5)
+    def tearDown(self):
+        """Stop web driver"""
+        self.driver.quit()
 
-title = driver.title
-print(title.encode('utf-8'))
+    def test_case_1(self):
+        """Find and click top-left logo button"""
+        try:
+            self.driver.get('https://www.oursky.com/')
+            el = self.driver.find_element_by_class_name('header__logo')
+            el.click()
+        except NoSuchElementException as ex:
+            self.fail(ex.msg)
 
-# html=driver.page_source
-# print(html)
+    def test_case_2(self):
+        """Find and click top-right Start your project button"""
+        try:
+            self.driver.get('https://www.oursky.com/')
+            el = self.driver.find_element_by_class_name("header__cta")
+            el.click()
+        except NoSuchElementException as ex:
+            self.fail(ex.msg)
 
-# Release memory
-driver.close()
-driver.quit()
-display.stop()
+if __name__ == '__main__':
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestTemplate)
+    unittest.TextTestRunner(verbosity=2).run(suite)

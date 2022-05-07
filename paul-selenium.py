483d2468bf7fa6c10237ba921434eb1a7595c291
paul-harness <56152762+paul-harness@users.noreply.github.com>
2022-05-07 07:53:09 -0400

    Update paul-selenium.py

--- paul-selenium.py
+++ paul-selenium.py
@@ -1,7 +1,21 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-chrome_options = Options()
-chrome_options.add_argument("--no-sandbox")
-chrome_options.add_argument("--headless")
-driver = webdriver.Chrome(options=chrome_options)
-driver.get('http://example.com')
+from selenium.webdriver.common.by import By
+
+
+driver = webdriver.Chrome()
+
+driver.get("${PLUGIN_URL}")
+
+driver.title # => "Google"
+
+driver.implicitly_wait(0.5)
+
+search_box = driver.find_element(By.NAME, "q")
+search_button = driver.find_element(By.NAME, "btnK")
+
+search_box.send_keys("${PLUGIN_SEARCHTERM")
+search_button.click()
+
+driver.find_element(By.NAME, "q").get_attribute("value") # => "Selenium"
+
+driver.quit()

6d8e909a1d5bdb03c6ad892227110c5b2f4e6e8c
paul-harness <56152762+paul-harness@users.noreply.github.com>
2022-05-07 08:03:23 -0400

    Update paul-selenium.py

--- paul-selenium.py
+++ paul-selenium.py
@@ -1,21 +1,6 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.options import Options
+from pyvirtualdisplay import Display
 
-
-driver = webdriver.Chrome()
-
-driver.get("${PLUGIN_URL}")
-
-driver.title # => "Google"
-
-driver.implicitly_wait(0.5)
-
-search_box = driver.find_element(By.NAME, "q")
-search_button = driver.find_element(By.NAME, "btnK")
-
-search_box.send_keys("${PLUGIN_SEARCHTERM")
-search_button.click()
-
-driver.find_element(By.NAME, "q").get_attribute("value") # => "Selenium"
-
-driver.quit()
+display = Display(visible=0, size=(800, 600))
+display.start()
+driver = webdriver.Firefox()

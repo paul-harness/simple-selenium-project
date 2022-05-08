5ee825399c9bc615fb0ba19d2c4fe666881a18ea
paul-harness <56152762+paul-harness@users.noreply.github.com>
2022-05-08 05:59:51 -0400

    Update paul-selenium.py

--- paul-selenium.py
+++ paul-selenium.py
@@ -2,12 +2,26 @@
 from selenium import webdriver
 import os
 
-URL = os.environ.get('PLUGIN_URL') 
+URL = os.environ.get('PLUGIN_URL')
+SEARCHTERM = 0s.environ.get('PLUGIN_SEARCHTERM')
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--disable-gpu')
 driver = webdriver.Chrome(chrome_options=chrome_options)
+
+# print page source for URL
 driver.get(URL)
 print(driver.page_source)
+
+# return TRUE if SEARCHTERM is in page source
+# Getting current URL source code 
+get_source = driver.page_source
+  
+# Text you want to search
+search_text = SEARCHTERM
+  
+# print True if text is present else False
+print(search_text in get_source)
+

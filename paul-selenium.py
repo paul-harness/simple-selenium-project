f05684bf484bb0bbcd9bcd2ff5be6c8ab7822967
paul-harness <56152762+paul-harness@users.noreply.github.com>
2022-05-08 05:49:51 -0400

    Update paul-selenium.py

--- paul-selenium.py
+++ paul-selenium.py
@@ -1,9 +1,10 @@
 # tutorial to get page source of google.com
 from selenium import webdriver
+URL = os.environ.get('PLUGIN_URL') 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--disable-gpu')
 driver = webdriver.Chrome(chrome_options=chrome_options)
-driver.get('"${PLUGIN_URL}"')
+driver.get('URL')
 print(driver.page_source)

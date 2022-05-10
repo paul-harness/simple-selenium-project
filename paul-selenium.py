693e5afc8744694c6cd4d4111cd84dedde7e953f
paul-harness <56152762+paul-harness@users.noreply.github.com>
2022-05-10 08:36:30 -0400

    Update paul-selenium.py

--- paul-selenium.py
+++ paul-selenium.py
@@ -1,9 +1,8 @@
 # tutorial to get page source of google.com
 from selenium import webdriver
-import os
-
-URL = os.environ.get('PLUGIN_URL')
-SEARCHTERM = os.environ.get('PLUGIN_SEARCHTERM')
+import pytest
+import sys
+from time import sleep
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--no-sandbox')
@@ -11,19 +10,28 @@ chrome_options.add_argument('--headless')
 chrome_options.add_argument('--disable-gpu')
 driver = webdriver.Chrome(chrome_options=chrome_options)
 
-# print page source for URL
-driver.get(URL)
-print(driver.page_source)
-
-# return TRUE if SEARCHTERM is in page source
-# Getting current URL source code 
-get_source = driver.page_source
-  
-# Text you want to search
-search_text = SEARCHTERM
-  
-# print True if text is present else False
-print('\n\n"',SEARCHTERM,'"',"in the page source of",URL,"?\n")
-print("TRUE or FALSE\n")
-print(search_text in get_source)
-
+def test_lambdatest_todo_app():
+    driver = webdriver.Chrome()
+    
+    driver.get('https://lambdatest.github.io/sample-todo-app/')
+    driver.maximize_window()
+ 
+    driver.find_element_by_name("li1").click()
+    driver.find_element_by_name("li2").click()
+ 
+    title = "Sample page - lambdatest.com"
+    assert title == driver.title
+ 
+    sample_text = "Happy Testing at LambdaTest"
+    email_text_field = driver.find_element_by_id("sampletodotext")
+    email_text_field.send_keys(sample_text)
+    sleep(5)
+ 
+    driver.find_element_by_id("addbutton").click()
+    sleep(5)
+ 
+    output_str = driver.find_element_by_name("li6").text
+    sys.stderr.write(output_str)
+    
+    sleep(2)
+    driver.close()

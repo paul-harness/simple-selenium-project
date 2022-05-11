95a2aa7617555204e3e6f557665de63a4112bf48
paul-harness <56152762+paul-harness@users.noreply.github.com>
2022-05-11 06:55:45 -0400

    Update fb-selenium.py

--- fb-selenium.py
+++ fb-selenium.py
@@ -14,24 +14,20 @@ class FacebookLogin():
         # Store credentials for login
         self.email = email
         self.password = password
-        if browser == 'Chrome':
-            # Use chrome
-            self.driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
-        elif browser == 'Firefox':
-            # Set it to Firefox
-        self.driver.get(LOGIN_URL)
+        driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
+        driver.get(LOGIN_URL)
         time.sleep(1) # Wait for some time to load
  
  
  
     def login(self):
-        email_element = self.driver.find_element_by_id('email')
+        email_element = driver.find_element_by_id('email')
         email_element.send_keys(self.email) # Give keyboard input
  
-        password_element = self.driver.find_element_by_id('pass')
+        password_element = driver.find_element_by_id('pass')
         password_element.send_keys(self.password) # Give password as input too
  
-        login_button = self.driver.find_element_by_id('loginbutton')
+        login_button = driver.find_element_by_id('loginbutton')
         login_button.click() # Send mouse click
  
         time.sleep(2) # Wait for 2 seconds for the page to show up
